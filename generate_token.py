9dd39576cacc37bb08fed678025830a74a8afbf1
jinmao115-max <jinmao115@gmail.com>
2026-06-10 08:56:27 +0900

    feat: ホテル最安値チェッカーを追加

---
+++ generate_token.py
@@ -0,0 +1,13 @@
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+
+flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+creds = flow.run_local_server(port=0)
+
+with open('token.json', 'w') as token:
+    token.write(creds.to_json())
+
+print("✅ token.json を生成しました！")
